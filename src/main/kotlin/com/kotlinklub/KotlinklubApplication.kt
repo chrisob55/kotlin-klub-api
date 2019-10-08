e18fbb365b8c9a439dbaabd9871bdbff0b27aa51
Christopher O'Brien <chris.obrien@digital.hmrc.gov.uk>
2019-10-08 15:22:54 +0100

    master - initial commit

---
+++ src/main/kotlin/com/kotlinklub/KotlinklubApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlinklub
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinklubApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinklubApplication>(*args)
+}
